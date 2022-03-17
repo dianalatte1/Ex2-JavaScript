b17f28e4e88ba371d386ad135fcb1f585842426e
Enrique Munguía <xemc@pm.me>
2022-03-17 00:57:32 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function sumar(params) {
+  let number = document.getElementById("asdf").value;
+  console.log(number);
+  let array = [];
+  let sum = 0;
+  for (let i = 0; i < number; i++) {
+    let n = Math.floor(Math.random() * 1000);
+    sum += n;
+    array.push(n);
+  }
+  console.log(array);
+  console.log(sum);
+}
